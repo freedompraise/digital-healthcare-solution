e3eafe9a3184f0b6a2265488be086e88259c2f70
Praise Freedom Dike <dikepraise119@gmail.com>
2023-11-24 01:18:32 +0100

    install django

---
+++ medication_adherence_project/medication_adherence_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MedicationAdherenceAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "medication_adherence_app"
