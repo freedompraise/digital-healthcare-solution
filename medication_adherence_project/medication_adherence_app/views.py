8f8db4ad8eac0905d0aec5b83adc26da5fdb59bf
Praise Freedom Dike <dikepraise119@gmail.com>
2023-11-25 12:04:03 +0100

    Remove unused imports and serializers

--- medication_adherence_project/medication_adherence_app/views.py
+++ medication_adherence_project/medication_adherence_app/views.py
@@ -1,19 +1,13 @@
 # medication_adherence_app/views.py
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from .models import CustomUser, HealthcareProvider, Patient
 from .serializers import (
     PatientSerializer,
-    HealthcareProviderSerializer,
     TokenObtainPairSerializer,
     CustomUserSerializer,
-    AdherenceReportSerializer,
-    CommunicationLogSerializer,
 )
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
