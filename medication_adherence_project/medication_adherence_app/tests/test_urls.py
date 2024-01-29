c510745c0aaf2d3e2f09b01fcfdfeb76cc01ccaf
Praise Freedom Dike <dikepraise119@gmail.com>
2024-01-29 07:01:45 +0100

    Add login and refresh URLs to test_urls.py

--- medication_adherence_project/medication_adherence_app/tests/test_urls.py
+++ medication_adherence_project/medication_adherence_app/tests/test_urls.py
@@ -7,8 +7,10 @@ from .utils import create_patient_user, create_provider_user
 class UrlsTestCase(APITestCase):
     def setUp(self):
         self.patient_user, self.patient = create_patient_user()
-
         self.provider_user, self.provider = create_provider_user()
+        self.login_url = reverse("token-create")
+        self.refresh_url = reverse("token-refresh")
+        self.patient_detail_url = reverse("patient-detail")
 
     def test_register_user_url(self):
         url = reverse("register-user")
@@ -29,13 +31,30 @@ class UrlsTestCase(APITestCase):
         }
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("access" in response.data)
+        return response.data.get("access")
 
+    def test_token_refresh_url(self):
+        login_data = {
+            "email": self.patient_user.email,
+            "password": "password",
+        }
+        login_response = self.client.post(self.login_url, login_data, format="json")
+        refresh_token = login_response.data.get("refresh")
+        refresh_data = {"refresh": refresh_token}
+        refresh_response = self.client.post(
+            self.refresh_url, refresh_data, format="json"
+        )
+        self.assertEqual(refresh_response.status_code, status.HTTP_200_OK)
 
-#     def test_patient_detail_url(self):
-#         url = reverse("patient-detail")
-#         self.client.force_login(self.patient_user)
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # def test_patient_detail_url(self):
+    #     access_token = self.test_token_obtain_url()
+    #     auth_header = {"Authorization": f"Bearer {access_token}"}
+    #     profile_response = self.client.get(
+    #         self.patient_detail_url, format="json", **auth_header
+    #     )
+    #     self.assertEqual(profile_response.status_code, status.HTTP_200_OK)
+
 
 #     def test_healthcare_provider_detail_url(self):
 #         url = reverse("healthcare-provider-detail")
